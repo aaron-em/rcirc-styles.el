b7a18da96c46a368987a22838e899ef54b85d873
Aaron Miller <me@aaron-miller.me>
2016-01-07 23:05:30 -0500

    add "default" to attr alist for easy completion in attribute insert

--- rcirc-styles.el
+++ rcirc-styles.el
@@ -91,7 +91,8 @@
   '(("\C-b" . bold)
     ("\C-]" . italic)
     ("\C-_" . underline)
-    ("\C-v" . inverse))
+    ("\C-v" . inverse)
+    ("\C-o" . default))
   "mIRC text attribute specification characters.")
 
 (defvar rcirc-styles-color-vector ["white"
@@ -268,7 +269,8 @@ mind when invoked outside that context."
         (let ((char (car pair))
               (face (cdr pair)))
           ;; If so, and if it's not C-o, toggle that attribute in `attrs'...
-          (when (looking-at char)
+          (when (and (not (eq face 'default))
+                     (looking-at char))
             (setq deletes (push `(,(point) . 1) deletes))
             (forward-char 1)
             (setq advanced t)
