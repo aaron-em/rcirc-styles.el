f313bf6a7470bed314b27c7a40558cb787d7bc67
Aaron Miller <me@aaron-miller.me>
2016-02-06 21:50:57 -0500

    Update metadata for 1.3.2

--- rcirc-styles.el
+++ rcirc-styles.el
@@ -1,6 +1,6 @@
 ;;; rcirc-styles.el --- support mIRC-style color and attribute codes
 
-;; Package-Version: 20160110.002
+;; Package-Version: 20160206.001
 ;; Copyright 2016 Aaron Miller <me@aaron-miller.me>
 ;; Package-Requires: ((cl-lib "0.5"))
 
